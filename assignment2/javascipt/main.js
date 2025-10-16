ac886e5313edb829fcbd5bc019360078bfb88878
Yadnyesh <yadnyeshkolte@gmail.com>
2025-10-16 17:02:53 +0530

    update

---
+++ assignment2/javascipt/main.js
@@ -0,0 +1,3 @@
+let arr1 = [10, 20, 12, 1, 2, 'yadnyesh', 'kolte', true, false, 'void'];
+
+console.log(arr1);
---
+++ javascipt/main.js
@@ -0,0 +1,5 @@
+let arr = new Array(10);
+for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(10);
+}  
+console.log(arr);
