22bbef027edd2dcfe1658978c17a94dbb7a801f9
Yadnyesh <168337313+yadnyeshkolte@users.noreply.github.com>
2025-10-26 16:31:16 +0530

    Create async01.js

---
+++ nodejs01/async/async01.js
@@ -0,0 +1,23 @@
+
+console.log("it is anync file to be rendered")
+
+const fetchData = new Promise((resolve, reject) => {
+    try{
+        setTimeout(() => {
+            const data = {id:12, name: "yadnyesh",city:"akola"}
+            resolve(data);
+        }, 2000)
+    }
+    catch(error){
+        reject("Error is there could you please resolve"+error.message)
+    }
+})
+
+fetchData.then(printingData).catch(err => {console.log("It is the error")});
+
+function printingData(data){
+
+    console.log(data)
+    console.log("The fetched data is "+JSON.stringify(data))
+    //console.log(data)
+}
