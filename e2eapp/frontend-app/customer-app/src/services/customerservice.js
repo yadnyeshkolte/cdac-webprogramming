4117b2bc113915caff98725332b690f58a8fed8d
Yadnyesh <yadnyeshkolte@gmail.com>
2025-11-01 17:07:45 +0530

    update

--- e2eapp/frontend-app/customer-app/src/services/customerservice.js
+++ e2eapp/frontend-app/customer-app/src/services/customerservice.js
@@ -7,6 +7,6 @@ export const CustomerService = {
     getAll : async() => (await axios.get(base_url)).data,
     getById : async(id) =>(await axios.get(`${base_url}/${id}`)).data,
     add : async(rec) =>(await axios.post(base_url, rec)).data,
-    update : async(rec) =>(await axios.put(`${base_url}/${id}`, rec)).data,
+    update : async(rec) =>(await axios.put(`${base_url}/${id}`), rec).data,
     delete : async(id) => (await axios.delete(`${base_url}/${id}`)).data,
 }
